8467a1e4c8ddb99cf51e8b3a34a55dc79304f593
DESKTOP-6R34HE7\Jiwon <jiwon72674@gmail.com>
2017-10-19 20:08:50 +0900

    메뉴화면 레이아웃 구성과 메뉴 자바 파일을 추가함!

--- dugeun/app/src/main/java/com/example/kmucs/dugeun/Duguen.java
+++ dugeun/app/src/main/java/com/example/kmucs/dugeun/Duguen.java
@@ -1,7 +1,9 @@
 package com.example.kmucs.dugeun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Duguen extends AppCompatActivity {
 
@@ -9,5 +11,35 @@ public class Duguen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_duguen);
+    }
+
+    public void menuinfo(View v){
+        Intent intent = new Intent(getApplicationContext(), we_are.class);
+        startActivity(intent);
+    }
+
+    public void country(View v){
+        Intent intent = new Intent(getApplicationContext(), countrymemo.class);
+        startActivity(intent);
+    }
+
+    public void memo(View v){
+        Intent intent = new Intent(getApplicationContext(), onememo.class);
+        startActivity(intent);
+    }
+
+    public void exchange(View v){
+        Intent intent = new Intent(getApplicationContext(), exchange.class);
+        startActivity(intent);
+    }
+
+    public void cash(View v){
+        Intent intent = new Intent(getApplicationContext(), cashbook.class);
+        startActivity(intent);
+    }
+
+    public void map(View v){
+        Intent intent = new Intent(getApplicationContext(), mappage.class);
+        startActivity(intent);
     }
 }
