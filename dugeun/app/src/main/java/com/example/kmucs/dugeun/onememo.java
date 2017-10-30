3884dc9fbed4f7378178324f7784aa58258e58ab
jiwon <jiwon72674@gmail.com>
2017-10-30 14:29:08 +0900

    업데이트

--- dugeun/app/src/main/java/com/example/kmucs/dugeun/onememo.java
+++ dugeun/app/src/main/java/com/example/kmucs/dugeun/onememo.java
@@ -1,14 +1,44 @@
 package com.example.kmucs.dugeun;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 
-public class onememo extends AppCompatActivity {
+public class onememo extends SQLiteOpenHelper{
+
+    private  static final String DB_NAME="EDBTDev";
+    private static final int DB_VER = 1;
+    public static final String DB_TABLE = "Task";
+    public static final String DB_COLUMN ="TaskName";
+
+
+    public onememo(Context context) {
+        super(context, DB_NAME, null, DB_VER);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_onememo);
+    public void onCreate (SQLiteDatabase db){
+        String query = String.format("CREATE TABLE %s (ID INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT NOT NULL", DB_TABLE, DB_COLUMN);
+        db.execSQL(query);
+
     }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        String query = String.format("DELETE TABLE IF EXISTS %s", DB_TABLE);
+        db.execSQL(query);
+        onCreate(db);
+    }
+
+    public void insertNewTask(String task){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DB_COLUMN, task);
+        db.insertWithOnConflict(DB_TABLE,null,values,SQLiteDatabase.CONFLICT_REPLACE);
+        db.close();
+    }
+
+    public void deleteTask(String t){}
 }
