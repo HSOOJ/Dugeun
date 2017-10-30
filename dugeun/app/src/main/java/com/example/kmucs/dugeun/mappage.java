d909f73b17cb5058880af44c5445ae48a2025f2e
jiwon <jiwon72674@gmail.com>
2017-10-30 13:38:14 +0900

    지도 코드 수정함. (지도 열림)

--- dugeun/app/src/main/java/com/example/kmucs/dugeun/mappage.java
+++ dugeun/app/src/main/java/com/example/kmucs/dugeun/mappage.java
@@ -1,49 +1,34 @@
 package com.example.kmucs.dugeun;
 
-import android.app.FragmentManager;
-
-import android.support.v7.app.AppCompatActivity;
-
 import android.os.Bundle;
-
-
+import android.support.v4.app.FragmentActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
-
 import com.google.android.gms.maps.GoogleMap;
-
-import com.google.android.gms.maps.MapFragment;
-
 import com.google.android.gms.maps.OnMapReadyCallback;
-
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-
 import com.google.android.gms.maps.model.MarkerOptions;
 
+    public class mappage extends FragmentActivity implements OnMapReadyCallback {
 
+        private GoogleMap mMap;
 
-public class mappage extends AppCompatActivity implements OnMapReadyCallback{
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_mappage);
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mappage);
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
 
-        FragmentManager fragmentManager = getFragmentManager();
-        MapFragment mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+            // Add a marker in Sydney, Australia, and move the camera.
+            LatLng SEOUL = new LatLng(37.527089, 127.028480);
+            mMap.addMarker(new MarkerOptions().position(SEOUL).title("Marker in SEOUL"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        }
     }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        LatLng SEOUL = new LatLng(37.52487, 126.92723);
-
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국 수도");
-        googleMap.addMarker(markerOptions);
-
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
-    }
-}
